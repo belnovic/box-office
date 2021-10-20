23935f5a58303c91e1bf7d738319f4c886c474af
Bella Novicki <bella.novicki@gmail.com>
2021-10-20 12:05:19 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+        Home page
+      </Route>
+
+      <Route exact path="/starred">
+        Starred
+      </Route>
+
+      {/* default error page */}
+      <Route>
+        Error: 404
+      </Route>
+    </Switch>
   );
 }
 
