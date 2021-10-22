27c1db6a7bae0b63500007f09494cdc31c9e3b01
Bella Novicki <bella.novicki@gmail.com>
2021-10-22 13:03:42 -0400

    added loading and error messages to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,22 +6,44 @@ const Show = () => {
 
     const { id } = useParams();
     const [show, setShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     // useEffect runs callback function whenever something inside array changes
-    // 
+    // whenever show id changes, retrieve a new show with new id
     useEffect(()=> {
 
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-            setShow(results);
-        })
+            setTimeout(() => {
+                if (isMounted){
+                    setShow(results);
+                    setIsLoading(false)
+                }
+            }, 2000);
+        }).catch(err => {
+            if (isMounted) {
+                setError(err.message);
+                setIsLoading(false);
+            }
+        });
 
         // cleanup function - is run right before the next callback function (right before component unmounts)
         return () => {
-
+            isMounted = false;
         }
     }, [id]);
 
-    console.log('show', show);
+    console.log(show);
+
+    if (isLoading) {
+        return <div>Data is being loaded</div>
+    }
+
+    if (error) {
+        return <div>Error occurred</div>
+    }
 
     return (
         <div>
